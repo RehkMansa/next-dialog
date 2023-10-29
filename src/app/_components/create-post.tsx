2bd017691ae8be0627cc8c1a817eb5d3ff4157a2
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2023-10-29 15:02:50 +0100

    Add style to button.

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -12,7 +12,7 @@ export function CreatePost() {
 
   return (
     <div>
-      <button>Post</button>
+      <button className="border border-gray-800 bg-gray-500 p-2">Post</button>
 
       <p>^ open dialog with post form</p>
     </div>
